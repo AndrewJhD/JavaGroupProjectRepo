45f0f4d5c7f02c15787ce97200516289bcc39a7f
ampontier1 <78193512+ampontier1@users.noreply.github.com>
2021-10-17 18:15:43 -0500

    Create/Edit
    Adding the Create and Edit Servlets.
    
    Had to add a method to OwnerHelper

--- src/main/java/controller/OwnerHelper.java
+++ src/main/java/controller/OwnerHelper.java
@@ -3,7 +3,9 @@ package controller;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.NoResultException;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
 import model.Owner;
 
@@ -21,6 +23,25 @@ public List<Owner> showAllOwners() {
 EntityManager em = emfactory.createEntityManager();
 List<Owner> allOwners = em.createQuery("SELECT s FROM Owner s").getResultList();
 return allOwners;
+}
+
+public Owner findOwner(String nameToLookUp) {
+	// TODO Auto-generated method stub
+	EntityManager em = emfactory.createEntityManager();
+	em.getTransaction().begin();
+	TypedQuery<Owner> typedQuery = em.createQuery("select ph from Park ph where ph.ParkName = :selectedName", Owner.class);
+	typedQuery.setParameter("selectedName", nameToLookUp);
+	typedQuery.setMaxResults(1);
+	
+	Owner foundOwner;
+	try {
+		foundOwner = typedQuery.getSingleResult();
+	} catch (NoResultException ex) {
+		foundOwner = new Owner(nameToLookUp);
+	}
+	em.close();
+		
+	return foundOwner;
 }
 }
 
